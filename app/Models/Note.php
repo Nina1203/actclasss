21548085615e4a01094be21ba941f4b6060dd9d9
Nina1203 <ninamariel894@gmail.com>
2024-04-15 17:30:54 -0600

    aaa

---
+++ app/Models/Note.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory;
+    
+    /*protected $fillable[
+    'id','title','content','active'
+    ];*/
+
+    protected $guarded =[];
+
+    static function todas_las_notas(){
+        return Note::where ('active', true) -> get ();
+    }
+}
